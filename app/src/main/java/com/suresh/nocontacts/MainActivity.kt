9f2636e34202260f567c5d0047d1108a925cd238
SalaSuresh <suresh.sala1992@gmail.com>
2023-07-25 12:44:19 +0530

    misc

--- app/src/main/java/com/suresh/nocontacts/MainActivity.kt
+++ app/src/main/java/com/suresh/nocontacts/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.suresh.nocontacts
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var imageDemo: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageDemo = findViewById(R.id.imageDemo)
     }
 }
